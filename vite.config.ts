6d1bd4177449e68ef079276b1f6ee7d549815f93
root <root@serverdandi.web.id>
2024-03-09 21:19:00 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bubududu-valentine",
   server: {
     host: 'ayangkucintakumaniskumybubu.anu.biz.id',
     port: 8080
